fcdb25b0981a76de8d73a83050f9f1e0c50ded46
plump_albert <plumpalbert@gmail.com>
2019-12-29 02:00:32 +0300

    Added function for DHCP message creation

--- src/dhcp-client.c
+++ src/dhcp-client.c
@@ -1,7 +1,12 @@
 #include "dhcp-client.h"
 
+#define MAC_ADDRESS_LENGTH 6
+#define MAX_RELAY_HOPS 30
+
+#define IP_BROADCAST 0xFFFFFFFF
+
 /**
- * Returns constant pointer to MAC address of the ${adapter_name}
+ * Returns constant pointer to MAC address of the adapter_name
  */
 unsigned char *const get_mac_address(const char *adapter_name) {
   struct ifreq request;
@@ -20,4 +25,29 @@ unsigned char *const get_mac_address(const char *adapter_name) {
   }
   unsigned char *const mac = (unsigned char *)request.ifr_hwaddr.sa_data;
   return mac;
+}
+
+/**
+ * Creates DHCP messages
+ */
+dhcp_packet *const create_dhcp_message(unsigned char const *mac_address,
+                                       const unsigned char *options,
+                                       int options_len) {
+  dhcp_packet *const packet = malloc(sizeof(uint8_t));
+  packet->op = DHCPDISCOVER;
+  packet->htype = HTYPE_ETHERNET_10MB;
+  packet->hlen = MAC_ADDRESS_LENGTH;
+  packet->hops = MAX_RELAY_HOPS;
+  packet->xid = (uint32_t)rand();
+  packet->secs = 0;
+  packet->flags = 0;
+  packet->ciaddr.s_addr = 0;
+  packet->yiaddr.s_addr = 0;
+  packet->siaddr.s_addr = 0;
+  packet->giaddr.s_addr = 0;
+  memcpy(packet->chaddr, mac_address, MAC_ADDRESS_LENGTH);
+  memset(packet->sname, 0, DHCP_SNAME_LEN);
+  memset(packet->file, 0, DHCP_FILE_LEN);
+  memcpy(packet->options, options, options_len);
+  return packet;
 }
--- src/dhcp-client.h
+++ src/dhcp-client.h
@@ -1,6 +1,8 @@
+#include <malloc.h>
 #include <net/if.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/fcntl.h>
 #include <sys/ioctl.h>
@@ -10,3 +12,5 @@
 #include "dhcp.h"
 
 unsigned char *const get_mac_address(const char *adapter_name);
+
+dhcp_packet *const create_dhcp_message();
