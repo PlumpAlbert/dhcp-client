c7adf0f5604bad7292de4cb3a13d796495d17e7b
plump_albert <plumpalbert@gmail.com>
2020-01-03 16:03:20 +0300

    Function for adding options to DHCP packet

--- include/dhcp-client.h
+++ include/dhcp-client.h
@@ -6,6 +6,15 @@
 
 #include "helpers.h"
 
+/*
+ * Adds option to the DHCP-packet
+ * @param type The type of the DHCP-option
+ * @param len The length of the data
+ * @param data The payload of the option
+ */
+dhcp_packet *add_option(dhcp_packet *packet, uint8_t type, uint8_t len,
+                        uint8_t *data);
+
 /**
  * Creates DHCP Discover message
  * @param adapter_name The name of the network adapter to use for DHCP message
--- src/dhcp-client.c
+++ src/dhcp-client.c
@@ -1,7 +1,28 @@
 #include "../include/dhcp-client.h"
+#include "../include/dhcp_option.h"
 
 #define MAC_ADDRESS_LENGTH 6
 #define MAX_RELAY_HOPS 30
+
+dhcp_packet *add_option(dhcp_packet *packet, uint8_t type, uint8_t len,
+                        uint8_t data[]) {
+  // Copying packet
+  dhcp_packet *p = malloc(sizeof(*packet));
+  memcpy(p, packet, sizeof(*packet));
+
+  // Creating DHCP-option
+  dhcp_option option = {.type = type, .length = len};
+  option.data = malloc(len);
+  memcpy(option.data, data, len);
+
+  // Allocating new options array
+  int8_t option_length = length(option);
+  unsigned long options_length = strlen((char *)packet->options);
+  p->options = malloc(option_length + options_length);
+  memcpy(p->options, packet->options, options_length);
+  memcpy(p->options, option_to_byte_array(option), option_length);
+  return p;
+}
 
 dhcp_packet *discovery(uint8_t adapter_type, const char *adapter_name) {
   // Preparation for packaging
@@ -22,9 +43,21 @@ dhcp_packet *discovery(uint8_t adapter_type, const char *adapter_name) {
   memcpy(packet->chaddr, mac, MAC_ADDRESS_LENGTH);
   memset(packet->sname, 0, DHCP_SNAME_LEN);
   memset(packet->file, 0, DHCP_FILE_LEN);
-  packet->options = malloc(sizeof(MAGIC_COOKIE) * sizeof(uint8_t));
-  memcpy(packet->options, MAGIC_COOKIE, sizeof(MAGIC_COOKIE));
-  //  memcpy(packet->options + (uint8_t)4, options, options_len);
-  //  memcpy(packet->options, options, options_len);
-  return packet;
+  //  // Adding options
+  //  dhcp_option option;
+  //  option.type = DHCP_OPTION_MESSAGE_TYPE;
+  //  option.length = 0x01;
+  //  option.data = malloc(sizeof(uint8_t));
+  //  option.data[0] = DHCPDISCOVER;
+  //  const uint8_t len = length(option);
+  //  packet->options = malloc(sizeof(MAGIC_COOKIE) + len);
+  //  memcpy(packet->options, MAGIC_COOKIE, sizeof(MAGIC_COOKIE));
+  //  memcpy(packet->options + sizeof(MAGIC_COOKIE),
+  //  option_to_byte_array(option),
+  //         len);
+  uint8_t *q = (uint8_t *){DHCPDISCOVER};
+  dhcp_packet *p = add_option(packet, DHCP_OPTION_MESSAGE_TYPE, 1, q);
+  free(packet->options);
+  free(packet);
+  return p;
 }
